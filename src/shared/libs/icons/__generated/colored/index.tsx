507d28443d163bacf156ddf9a0dc27e0d816fa4e
ARSync <63187302+Async-Reaper@users.noreply.github.com>
2023-03-02 03:03:04 +0000

    create top part

--- src/shared/libs/icons/__generated/colored/index.tsx
+++ src/shared/libs/icons/__generated/colored/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import {SVGAttributes} from 'react';
+import {LineIcon as line} from './Line';
 import {LogoIcon as logo} from './Logo';
 import {StatusOnlineIcon as status_online} from './StatusOnline';
 import {WaveIcon as wave} from './Wave';
 
 export type IconName =
+  | 'line'
   | 'logo'
   | 'status_online'
   | 'wave';
@@ -12,6 +14,7 @@ export type IconName =
 export const iconSet: {
   [key in IconName]: React.FC<SVGAttributes<SVGElement> & {size?: number}>;
 } = {
+  line,
   logo,
   status_online,
   wave
--- src/widgets/video-lessons/index.tsx
+++ src/widgets/video-lessons/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { ColoredIcon } from 'shared/libs/icons';
+import { Typography } from 'shared/ui';
+import cls from './styles.module.scss';
 
 const Component = () => (
-   <div />
+   <div className={cls.video__lessons}>
+      <div className={cls.video__lessonsTopIcon}>
+         <ColoredIcon name='line' width={64} height={4} />
+      </div>
+      <div className={cls.title}>
+         <Typography tag='h1' variant='h1' uppercase>
+            Talent Q: 6 шагов к успеху
+         </Typography>
+      </div>
+      <div className={cls.videoWrapper}>
+         <div className={cls.videoTitle}>
+            <Typography tag='h2' variant='h2'>
+               Изучите видеоуроки
+            </Typography>
+         </div>
+         <div>
+            <div className="video">
+               
+            </div>
+         </div>
+      </div>
+   </div>
 );
 
 export const VideoLessons = React.memo(Component);

78f32374170913e7e7738644d13c00bed9c7044b
ARSync <63187302+Async-Reaper@users.noreply.github.com>
2023-03-02 01:30:09 +0000

    fix bottom-icon

--- src/shared/libs/icons/__generated/colored/Wave.tsx
+++ src/shared/libs/icons/__generated/colored/Wave.tsx
@@ -12,15 +12,15 @@ export const WaveIcon = React.memo(
     return (
       <svg width={size} height={size} fill="none" {...props}>
         <path
-          d="M-322 235.498c66.851-38.704 424.305-79.917 574-39.998 187.119 49.898 463 166.927 696 148C1181 324.573 1625 47.02 1720 0v731H-322V235.498z"
+          d="M0 235.498c66.851-38.704 424.305-79.917 574-39.998 187.119 49.898 463 166.927 696 148C1503 324.573 1947 47.02 2042 0v731H0V235.498z"
           fill="url(#wave_svg__a)"
         />
         <defs>
           <linearGradient
             id="wave_svg__a"
-            x1={699}
+            x1={1021}
             y1={243.5}
-            x2={699}
+            x2={1021}
             y2={638}
             gradientUnits="userSpaceOnUse"
           >

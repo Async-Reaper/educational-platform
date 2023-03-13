5928e37eb762a0daa93471c9f59e6365adb42cfc
ARSync <63187302+Async-Reaper@users.noreply.github.com>
2023-03-13 08:25:24 +0000

    restyle user

--- src/entities/user/ui/index.tsx
+++ src/entities/user/ui/index.tsx
@@ -6,6 +6,7 @@ import cls from './styles.module.scss';
 
 const Component = () => {
   const user: UserType = {
+    id: 34534,
     avatar: ava,
     firstname: '',
     surname: '',
@@ -13,20 +14,39 @@ const Component = () => {
   };
   return (
      <div className={cls.user_info}>
-        <Avatar src={user.avatar} />
-        <Typography>
-           Имя:
-           {user.firstname}
-        </Typography>
-        <Typography>
-           Фамилия:
-           {user.surname}
-        </Typography>
-        <Typography>
-           <span>Телефон:</span>
-           {' '}
-           {user.phone}
-        </Typography>
+        <div className={cls.top_info_part}>
+           <Avatar src={user.avatar} variant='xl' />
+           <Typography variant='h1' color='violet-primary'>
+              #
+              {user.id}
+           </Typography>
+        </div>
+        <div className={cls.other__info}>
+           <div className={cls.info_item}>
+              <Typography variant='h3'>
+                 Имя:
+              </Typography>
+              <Typography variant='h3' color='violet-primary'>
+                 {user.firstname}
+              </Typography>
+           </div>
+           <div className={cls.info_item}>
+              <Typography variant='h3'>
+                 Фамилия:
+              </Typography>
+              <Typography variant='h3' color='violet-primary'>
+                 {user.surname}
+              </Typography>
+           </div>
+           <div className={cls.info_item}>
+              <Typography variant='h3'>
+                 Телефон:
+              </Typography>
+              <Typography variant='h3' color='violet-primary'>
+                 {user.phone}
+              </Typography>
+           </div>
+        </div>
      </div>
   );
 };
